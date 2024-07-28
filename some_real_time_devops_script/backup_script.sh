bcf66db6693570204ba3c2720c52715ab55b1ddc
sid14581 <siddhardh.kbv@gmail.com>
2024-07-28 12:21:35 +0000

    added backup roation code

--- some_real_time_devops_script/backup_script.sh
+++ some_real_time_devops_script/backup_script.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-SRC="/home/sid/Documents/my_jenkins_project/Small-scale-urban-services/"
-DES="/home/sid/linux_real_time_devops_work/backup_script_destination"
+###########################################################################################################
+## 
+##  Author: K Siddharth Reddy
+##
+##  Date Created: 07-11-2024
+##  Date Modified: 07-28-2024
+##
+##  Usage: database backup
+##  Description: Backuping the database in the backup folderr with backup  date and time
+##
+################################################################################################
+
+
+SRC="/home/ubuntu/Shell_Scripting_Practice/some_real_time_devops_script/"
+DES="/home/ubuntu/Shell_Scripting_Practice/backup_script_destination/"
 
 DATE=$(date +%Y-%m-%d-%T)
 
@@ -10,3 +23,30 @@ backup="$DES/$DATE"
 cp -r $SRC $backup
 
 
+backups=($(ls ../backup_script_destination/ -t))
+
+echo " "
+echo "List of the backups-->"
+for backup in "${backups[@]}"; do  
+   echo "${backup}"
+done
+echo " "
+
+if [[ "${#backups[@]}" -gt 5 ]]; then
+  for backup in "${backups[@]:5}"; do 
+       echo "backup file $backup is removed "
+       rm -dfr "../backup_script_destination/$backup"
+  done
+fi
+echo " "
+echo "new backup file ${backups[0]} is added"
+echo " "
+
+backups=($(ls ../backup_script_destination/ -t))
+
+echo " "
+echo "List of the backups-->"
+for backup in "${backups[@]}"; do
+   echo "${backup}"
+done
+echo " "
